c9fe59d69ee8bf4cfc5eb3d3c20416ad4b45c1a2
Leonardo Flores <contato@leonardocouy.com>
2019-05-05 15:36:09 -0300

    Rearrange setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-ln -snfv $PWD/.presets/ $HOME/.presets
+ln -sv $PWD/.presets/ $HOME
 ln -sv $PWD/.bash_profile $HOME/.bash_profile
 ln -sv $PWD/.bash_prompt $HOME/.bash_prompt
 ln -sv $PWD/.bashrc $HOME/.bashrc
 ln -sv $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sv $PWD/.inputrc $HOME/.inputrc
 ln -sv $PWD/.pryrc $HOME/.pryrc
+ln -sv $PWD/.gitconfig $HOME/.gitconfig
+ln -sv $PWD/.gitignore_global $HOME/.gitignore_global
+ln -sv $PWD/.tmux.conf $HOME/.tmux.conf
