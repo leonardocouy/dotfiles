e52d959e827730e2a512e421cca2a4d68ed4f4ef
Leonardo Flores <leonardoflores@MacBook-Pro-de-Leonardo.local>
2019-05-05 22:47:30 -0300

    Force overriding on create symlink files

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 ln -sv $PWD/.presets/ $HOME
-ln -sv $PWD/.bash_profile $HOME/.bash_profile
-ln -sv $PWD/.bash_prompt $HOME/.bash_prompt
-ln -sv $PWD/.bashrc $HOME/.bashrc
-ln -sv $PWD/.tmux.conf $HOME/.tmux.conf
-ln -sv $PWD/.inputrc $HOME/.inputrc
-ln -sv $PWD/.pryrc $HOME/.pryrc
-ln -sv $PWD/.gitconfig $HOME/.gitconfig
-ln -sv $PWD/.gitignore_global $HOME/.gitignore_global
-ln -sv $PWD/.tmux.conf $HOME/.tmux.conf
+ln -sfv $PWD/.bash_profile $HOME/.bash_profile
+ln -sfv $PWD/.bash_prompt $HOME/.bash_prompt
+ln -sfv $PWD/.bashrc $HOME/.bashrc
+ln -sfv $PWD/.tmux.conf $HOME/.tmux.conf
+ln -sfv $PWD/.inputrc $HOME/.inputrc
+ln -sfv $PWD/.pryrc $HOME/.pryrc
+ln -sfv $PWD/.gitconfig $HOME/.gitconfig
+ln -sfv $PWD/.gitignore_global $HOME/.gitignore_global
