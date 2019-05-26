34a95c913a77ff748730cce427cbc15f53c54ebb
Leonardo Flores <contato@leonardocouy.com>
2019-05-26 14:27:14 -0300

    Add linux setup dotfiles

---
+++ linux/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sv $PWD/.presets $HOME/
+ln -sfv $PWD/.bash_profile $HOME/.bash_profile
+ln -sfv $PWD/.tmux.conf $HOME/.tmux.conf
+ln -sfv $PWD/.pryrc $HOME/.pryrc
+ln -sfv $PWD/.gitconfig $HOME/.gitconfig
+ln -sfv $PWD/.gitignore_global $HOME/.gitignore_global
